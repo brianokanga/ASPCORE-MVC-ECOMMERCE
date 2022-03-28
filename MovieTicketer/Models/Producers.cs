f12eb23bf2b8b9485f6024e2a9f944cd6e8975ac
Brian <briannokanga@gmail.com>
2022-03-28 11:57:25 +0300

    Added Producers model

--- MovieTicketer/Models/Producers.cs
+++ MovieTicketer/Models/Producers.cs
@@ -1,7 +1,14 @@
-﻿namespace MovieTicketer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicketer.Models
 {
     public class Producers
     {
+        [Key]
+        public int Id { get; set; }
 
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
     }
 }

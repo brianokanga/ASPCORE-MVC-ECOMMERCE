e9b17cfba49fe07f253469511a3c8f6b7479c6ca
Brian <briannokanga@gmail.com>
2022-04-07 16:34:03 +0300

    Added IActorsService

---
+++ MovieTicketer/Data/Services/ActorsService.cs
@@ -0,0 +1,28 @@
+﻿using MovieTicketer.Models;
+using System.Collections.Generic;
+
+namespace MovieTicketer.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        public void Add(Actor actor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Actor Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MovieTicketer/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using MovieTicketer.Models;
+using System.Collections.Generic;
+
+namespace MovieTicketer.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor Get(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+    }
+}

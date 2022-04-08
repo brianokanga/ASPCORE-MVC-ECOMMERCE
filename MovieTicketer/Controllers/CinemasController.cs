a91ebb6a9d68e18e1257eb4bd51a776a64f5d82d
Brian <briannokanga@gmail.com>
2022-04-08 21:00:07 +0300

    Added cinemas controller

--- MovieTicketer/Controllers/CinemasController.cs
+++ MovieTicketer/Controllers/CinemasController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTicketer.Data;
+using System.Threading.Tasks;
 
 namespace MovieTicketer.Controllers
 {
     public class CinemasController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Cinemas.ToListAsync();
             return View();
         }
     }

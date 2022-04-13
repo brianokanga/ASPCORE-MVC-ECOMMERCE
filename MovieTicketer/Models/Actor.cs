451dba3a1fcb6c06fe7b4e1a1376433695e84ec3
Brian <briannokanga@gmail.com>
2022-04-13 17:04:23 +0300

    Resolved display error for Actors, producers and cinemas

--- MovieTicketer/Models/Actor.cs
+++ MovieTicketer/Models/Actor.cs
@@ -12,7 +12,7 @@ namespace MovieTicketer.Models
         [Required(ErrorMessage ="Profile picture is required")]
         public string ProfilePictureURL { get; set; }
 
-        [Display(Name = "FullName")]
+        [Display(Name = "Full Name")]
         [StringLength(50, MinimumLength = 3, ErrorMessage ="String length must be between 3 and 50 chars")]
         [Required(ErrorMessage = "FullName is required")]
         public string FullName { get; set; }

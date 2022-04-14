50b1acf6af8ba0bb4b6f12816f6d2e3ef8e5305b
Brian <briannokanga@gmail.com>
2022-04-14 16:55:56 +0300

    Added IEntityRepository

--- MovieTicketer/Data/Base/IEntityBaseRepository.cs
+++ MovieTicketer/Data/Base/IEntityBaseRepository.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using MovieTicketer.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MovieTicketer.Data.Base
 {
-    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
-    {
-        //Task<IEnumerable<T>> GetAll();
-        //Actor GetById(int id);
-        //void Add(Actor actor);
-        //Actor Update(int id, Actor newActor);
-        //void Delete(int id);
-    }
+	public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+	{
+		Task<IEnumerable<T>> GetAll();
+		T GetById(int id);
+		void Add(Actor T);
+		Actor Update(int id, Actor entity);
+		void Delete(int id);
+	}
 }

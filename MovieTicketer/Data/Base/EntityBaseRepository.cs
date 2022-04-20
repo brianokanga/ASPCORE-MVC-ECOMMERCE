b137765c9c2a36f8891d777ba4c16ff7b75856ca
Brian <briannokanga@gmail.com>
2022-04-20 15:34:39 +0300

    Added Generic GetAll and GetById methods to EntitybaseRepository

--- MovieTicketer/Data/Base/EntityBaseRepository.cs
+++ MovieTicketer/Data/Base/EntityBaseRepository.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MovieTicketer.Data.Base
 {
 	public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
 	{
+		private readonly AppDbContext _context;
+
+		public EntityBaseRepository(AppDbContext context)
+		{
+			_context = context;
+		}
 		public Task AddAsync(T Entity)
 		{
 			throw new System.NotImplementedException();
@@ -15,14 +22,16 @@ namespace MovieTicketer.Data.Base
 			throw new System.NotImplementedException();
 		}
 
-		public Task<IEnumerable<T>> GetAllAsync()
+		public async Task<IEnumerable<T>> GetAllAsync()
 		{
-			throw new System.NotImplementedException();
+			var results = await _context.Set<T>().ToListAsync();
+			return results;
 		}
 
-		public Task<T> GetByIdAsync(int id)
+		public async Task<T> GetByIdAsync(int id)
 		{
-			throw new System.NotImplementedException();
+			var results = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+			return results;
 		}
 
 		public Task<T> UpdateAsync(int id, T Entity)

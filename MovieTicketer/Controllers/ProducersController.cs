fb24dde406136f352d5e9cbe42ab3fec3640dded
Brian <briannokanga@gmail.com>
2022-04-20 17:14:14 +0300

    Added by Id method for producers

--- MovieTicketer/Controllers/ProducersController.cs
+++ MovieTicketer/Controllers/ProducersController.cs
@@ -1,22 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieTicketer.Data;
+using MovieTicketer.Data.Services;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MovieTicketer.Controllers
 {
-    public class ProducersController : Controller
-    {
-        private readonly AppDbContext _context;
-        public ProducersController(AppDbContext context)
-        {
-            _context = context;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var allProducers = await _context.Producers.ToListAsync();
-            return View(allProducers);
-        }
-    }
+	public class ProducersController : Controller
+	{
+		private readonly IProducersService  _service;
+		public ProducersController(IProducersService service)
+		{
+			_service = service;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var allProducers = await _service.GetAllAsync();
+			return View(allProducers);
+		}
+
+		//GET : producers/Details/1
+		public async Task<IActionResult> Details(int id)
+		{
+			var prodcerDetails = await _service.GetByIdAsync(id);
+			if (prodcerDetails == null) return View("NotFound");
+			return View(prodcerDetails);
+		}
+	}
 }

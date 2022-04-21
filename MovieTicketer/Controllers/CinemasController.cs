7e836bd85bcb7925552366f70620435835c9e156
Brian <briannokanga@gmail.com>
2022-04-21 15:11:21 +0300

    Added the cinemas index ActionMethod

--- MovieTicketer/Controllers/CinemasController.cs
+++ MovieTicketer/Controllers/CinemasController.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieTicketer.Data;
+using MovieTicketer.Data.Services;
 using System.Threading.Tasks;
 
 namespace MovieTicketer.Controllers
 {
     public class CinemasController : Controller
     {
-        private readonly AppDbContext _context;
-        public CinemasController(AppDbContext context)
+        private readonly ICinemasService _service;
+        public CinemasController(ICinemasService service)
         {
-            _context = context;
+            _service = service;
         }
+
         public async Task<IActionResult> Index()
         {
-            var allCinemas = await _context.Cinemas.ToListAsync();
+            var allCinemas = await _service.GetAllAsync();
             return View(allCinemas);
         }
     }

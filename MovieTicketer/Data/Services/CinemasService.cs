8353feccd58289a60bf25b9428016a6f6d0aa6ad
Brian <briannokanga@gmail.com>
2022-04-21 15:03:37 +0300

    Added CinemasService

--- MovieTicketer/Data/Services/CinemasService.cs
+++ MovieTicketer/Data/Services/CinemasService.cs
@@ -1,6 +1,11 @@
-﻿namespace MovieTicketer.Data.Services
+﻿using MovieTicketer.Data.Base;
+using MovieTicketer.Models;
+
+namespace MovieTicketer.Data.Services
 {
-	public class CinemasService
+	public class CinemasService : EntityBaseRepository<Cinema>, ICinemasService
 	{
+		public CinemasService(AppDbContext context)
+			: base(context) { }
 	}
 }
